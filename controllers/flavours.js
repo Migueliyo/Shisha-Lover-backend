f63e85200945b24767d021f021e5840275eca90e
Migueliyo <migueliyocolmenero@gmail.com>
2023-10-11 12:12:19 +0200

    Correcting mistakes /flavours

--- controllers/flavours.js
+++ controllers/flavours.js
@@ -19,15 +19,19 @@ export class FlavourController {
   }
 
   create = async (req, res) => {
-    const result = validateFlavour(req.body)
+    try {
+      const result = validateFlavour(req.body)
 
-    if (!result.success) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
+      if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+      }
+
+      const newFlavour = await this.flavourModel.create({ input: result.data })
+
+      res.status(201).json(newFlavour)
+    } catch (error) {
+      res.status(400).json({ message: error.message })
     }
-
-    const newFlavour = await this.flavourModel.create({ input: result.data })
-
-    res.status(201).json(newFlavour)
   }
 
   delete = async (req, res) => {
@@ -43,16 +47,20 @@ export class FlavourController {
   }
 
   update = async (req, res) => {
-    const result = validatePartialFlavour(req.body)
+    try {
+      const result = validatePartialFlavour(req.body)
 
-    if (!result.success) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
+      if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+      }
+  
+      const { id } = req.params
+  
+      const updatedFlavour = await this.flavourModel.update({ id, input: result.data })
+  
+      return res.json(updatedFlavour)
+    } catch (error) {
+      res.status(400).json({ message: error.message })
     }
-
-    const { id } = req.params
-
-    const updatedFlavour = await this.flavourModel.update({ id, input: result.data })
-
-    return res.json(updatedFlavour)
   }
 }
--- models/flavour.js
+++ models/flavour.js
@@ -22,7 +22,7 @@ export class FlavourModel {
         [lowerCaseBrand]
       )
 
-      return [flavours]
+      return flavours
     }
 
     const [flavours] = await connection.query(
@@ -51,7 +51,7 @@ export class FlavourModel {
     } = input
 
     const [brandIdResult] = await connection.query('SELECT id FROM brands WHERE name = ?' , brand)
-    const brandId = brandIdResult[0].id;
+    const brandId = brandIdResult[0].id
 
     try {
       await connection.query(
@@ -74,61 +74,61 @@ export class FlavourModel {
     try {
       const [result] = await connection.query(
         'DELETE FROM flavours WHERE id = ?', id
-      );
+      )
   
       if (result.affectedRows > 0) {
-        return true;
+        return true
       } else {
-        return false;
+        return false
       }
     } catch (error) {
-      throw new Error('Error deleting the flavour');
+      throw new Error('Error deleting the flavour')
     }
   }
   
   update = async ({ id, input }) => {
-    const { name, description, brand } = input;
+    const { name, description, brand } = input
   
     try {
       const [existingFlavour] = await connection.query(
         'SELECT * FROM flavours WHERE id = ?', [id]
-      );
+      )
   
       if (existingFlavour.length === 0) {
-        return {message: 'Flavour not found'};
+        throw new Error('Mix not found')
       }
   
-      const updateFields = [];
-      const updateValues = [];
+      const updateFields = []
+      const updateValues = []
   
       if (name !== undefined) {
-        updateFields.push('name = ?');
-        updateValues.push(name);
+        updateFields.push('name = ?')
+        updateValues.push(name)
       }
       
       if (description !== undefined) {
-        updateFields.push('description = ?');
-        updateValues.push(description);
+        updateFields.push('description = ?')
+        updateValues.push(description)
       }
   
       if (brand !== undefined) {
-        updateFields.push('brand_id = ?');
+        updateFields.push('brand_id = ?')
         const [brandIdResult] = await connection.query(
           'SELECT id FROM brands WHERE name = ?', [brand]
-        );
+        )
         
         if (brandIdResult.length === 0) {
-          return {message: 'Brand not found'};
+          throw new Error('Brand not found')
         }
-        const brandId = brandIdResult[0].id;
-        updateValues.push(brandId);
+        const brandId = brandIdResult[0].id
+        updateValues.push(brandId)
       }
   
       if (updateFields.length > 0) {
         const [result] = await connection.query(
           `UPDATE flavours SET ${updateFields.join(', ')} WHERE id = ?`,
           [...updateValues, id]
-        );
+        )
 
         // Verifica si se actualizó al menos una fila
         if (result.affectedRows > 0) {
@@ -136,14 +136,14 @@ export class FlavourModel {
             'SELECT * FROM flavours WHERE id = ?',
             [id]
           )
-          return flavours[0];
+          return flavours[0]
         } 
       } else {
-        return {message: 'No se han introducido datos'};
+        throw new Error('No data entered')
       }
 
     } catch (error) {
-      throw new Error('Error updating the flavour');
+      throw new Error('Error updating the flavour')
     }
   }
   
